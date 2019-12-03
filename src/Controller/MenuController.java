af4d6127b13bceb5052d24729fd792037968a5fd
Nizar Rezaigui <nizar.rezaigui@alumni.univ-avignon.fr>
2019-12-03 12:54:18 +0100

    a faire

---
+++ src/Controller/MenuController.java
@@ -0,0 +1,70 @@
+package Controller;
+
+
+import Grossiste.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.application.Platform;
+
+
+
+public class MenuController implements Initializable{
+
+	Main main = Main.getInstance();
+
+    @FXML
+    private AnchorPane fenetre;
+    @FXML
+    private Button commerce;
+    @FXML
+    private Button quit;
+    @FXML
+    private Button add;
+
+	
+    @FXML
+	void ajouter(ActionEvent event) throws IOException{
+    	Main main = Main.getInstance();
+    	
+    	FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../View/Produit.fxml"));
+		main.setRoot(loader.load());
+
+		Scene scene = new Scene(main.getRoot());
+    	main.getWindow().setScene(scene);
+    	main.getWindow().show();
+
+	}
+    
+
+    
+	
+    @FXML
+    void handleQuit(ActionEvent event)
+    {
+    	System.exit(0);
+    }
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1)
+	{
+		this.quit.setCancelButton(true);
+		Main.getInstance().getWindow().setOnCloseRequest( event ->
+		{
+			Platform.exit();
+			System.exit(0);
+		});
+	}
+	
+
+}
---
+++ src/Grossiste/Main.java
@@ -0,0 +1,92 @@
+package Grossiste;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import javafx.scene.layout.AnchorPane;
+
+
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+
+
+public class Main extends Application {
+	
+	
+	private Stage window;
+	private AnchorPane root;
+	private static volatile Main instance = null;
+	
+	public Main(){
+	    super();
+	    synchronized(Main.class){
+	        if(instance != null) throw new UnsupportedOperationException(
+	                getClass()+" is singleton but constructor called more than once");
+	        instance = this;
+	    }
+	}
+	@Override
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		window.setTitle("Grossiste CERI");	
+
+		try
+		{
+			// Permet de charger le menuPrincipal.fxml
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../View/menuView.fxml"));
+			root = (AnchorPane) loader.load();
+
+			// Affiche la scne du menu
+			Scene scene = new Scene(root);
+			window.setScene(scene);
+			window.setResizable(false);
+			window.show();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.err.println("Impossible d'afficher le menu");
+		}
+	}
+	
+	public final static Main getInstance()
+	{
+		if(Main.instance == null)
+		{
+			synchronized(Main.class)
+			{
+				if(Main.instance == null)
+				{
+					Main.instance = new Main();
+				}
+			}
+		}
+		return Main.instance;
+	}
+	
+
+	public Stage getWindow() {
+		return window;
+	}
+
+	public void setWindow(Stage window) {
+		this.window = window;
+	}
+
+	public AnchorPane getRoot() {
+		return root;
+	}
+
+	public void setRoot(AnchorPane root) {
+		this.root = root;
+	}
+
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+}
